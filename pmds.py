e4f91632fa27d0457ca273b17c910b0e7c616924
minhvu <vie.minhvu@gmail.com>
2020-09-29 12:19:20 +0200

    WIP loss for one pair

--- pmds.py
+++ pmds.py
@@ -1,11 +1,11 @@
 # Simple probabilistic MDS with jax
 from functools import partial
-from itertools import product
+from itertools import combinations
 import numpy as np
 
 import jax
 import jax.numpy as jnp
-from jax import random, grad, jit, vmap
+from jax import random
 from jax.scipy.special import xlogy, i0e, i1e
 from jax.test_util import check_grads
 from scipy.special import ive
@@ -50,48 +50,47 @@ def _ncx2_pdf(x, nc):  # df=2
     return jnp.exp(_ncx2_log_pdf(x, nc))
 
 
-def pmds(dists, n_samples=100, random_state=42, epoch=50):
-    dists = dists.reshape(-1)
+def pmds(dists, n_samples=100, random_state=42, lr=1e-2, epochs=5):
     n_components = 2
 
-    key_m, key_s = random.split(random.PRNGKey(0))
+    key_m, key_s = random.split(random.PRNGKey(random_state))
     mu = random.normal(key_m, (n_samples, n_components))
-    sigma_square = jax.nn.softplus(1e-2 * np.ones(n_samples))
-    params = (mu, sigma_square)
+    sigma_square = jax.nn.softplus(1e-4 * np.ones(n_samples))
 
-    all_pairs = list(product(range(n_samples), repeat=2))
-    idx0, idx1 = list(zip(*all_pairs))
+    all_pairs = list(combinations(range(n_samples), 2))
 
-    def loss(mu, sigma_square):
-        # nc = jnp.linalg.norm(mu[idx0] - mu[idx1]) / (
-        #     sigma_square[idx0] + sigma_square[idx1]
-        # )
-        # TODO optim for i != j
-        nc = jnp.stack(
-            [
-                jnp.linalg.norm(mu[i] - mu[j]) / (sigma_square[i] + sigma_square[j])
-                for i, j in all_pairs
-            ]
-        )
-        print(nc.shape)
-        nllh = -jnp.sum(_ncx2_log_pdf(x=dists, nc=nc))
-        print(nllh)
-        return nllh
+    # loss function for each pair
+    def loss_one_pair(params, d):
+        mu_i, mu_j, s_i, s_j = params
+        nc = jnp.sum((mu_i - mu_j) ** 2) / (s_i + s_j)
+        return _ncx2_log_pdf(x=d, nc=nc)
 
-    grad_func = grad(loss, (0, 1))
+    # make sure the gradient is auto calculated correctly
+    d = dists[1][4]
+    params = [mu[0], mu[1], sigma_square[0], sigma_square[1]]
+    check_grads(loss_one_pair, (params, d), order=1)
 
-    def update(params, step_size=0.1):
-        grads = grad_func(*params)
-        # TODO check NaN
-        return (params[0] - step_size * grads[0], params[1] - step_size * grads[1])
+    loss_grad = jax.jit(jax.grad(loss_one_pair))
+    loss_and_grad = jax.jit(jax.value_and_grad(loss_one_pair))
 
-    for i in range(epoch):
-        print(i)
-        params = update(params)
-        if i % 10 == 0:
-            print(loss(*params))
+    N = len(all_pairs)
+    for epoch in range(epochs):
+        loss = 0.0
+        for i, j in all_pairs:
+            params = [mu[i], mu[j], sigma_square[i], sigma_square[j]]
+            d = dists[i][j]
+            # grads = loss_grad(params, d)
+            loss_p, grads = loss_and_grad(params, d)
+            loss = loss + loss_p
 
-    return params
+            # think to update after each pair, or update in batch, or update in epoch
+            # preform update using jax.ops.index_add (to accumulate gradient)
+            mu = jax.ops.index_add(mu, [i, j], [grads[0], grads[1]])
+            sigma_square = jax.ops.index_add(sigma_square, [i, j], [grads[2], grads[3]])
+
+        print(epoch, loss / N)
+
+    return mu, sigma_square
 
 
 if __name__ == "__main__":
--- run_pmds.py
+++ run_pmds.py
@@ -1,8 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy.lib.npyio import load
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_digits, load_wine
 from sklearn.utils import shuffle
+from sklearn.manifold import MDS
 
 from scipy.spatial.distance import pdist, squareform
 from pmds import pmds
@@ -11,9 +12,16 @@ from pmds import pmds
 def run_pdms(D, N, labels=None):
     Z, Z_var = pmds(D, n_samples=N)
 
-    plt.figure(figsize=(4, 4))
-    plt.scatter(*Z.T, c=labels)
-    plt.savefig("plots/test0.png")
+    # original MDS
+    Z0 = MDS(metric="precomputed").fit_transform(D)
+
+    fig, [ax0, ax1] = plt.subplots(1, 2, figsize=(10, 4))
+    ax0.set_title("Original MDS")
+    ax0.scatter(*Z0.T, c=labels, cmap="tab10")
+
+    ax1.set_title("Probabilistic MDS")
+    ax1.scatter(*Z.T, c=labels, cmap="tab10")
+    fig.savefig("plots/test0.png")
 
 
 if __name__ == "__main__":
